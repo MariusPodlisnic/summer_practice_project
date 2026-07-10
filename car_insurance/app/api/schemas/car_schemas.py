db41753e81041d5157dd6351c716c4d539e22588
unknown <marius.podlisnic@student.usv.ro>
2026-07-10 14:46:02 +0300

    Revert "Merge pull request #11 from MariusPodlisnic/feature/list_cars_with_owners"
    This reverts commit b93346c5c64ba53d0d08354b30b92e7a8a95d8bd, reversing
    changes made to 975e6573fd28b2332e31973c8608c620b86328f2.

--- car_insurance/app/api/schemas/car_schemas.py
+++ car_insurance/app/api/schemas/car_schemas.py
@@ -5,11 +5,6 @@ from pydantic import BaseModel, ConfigDict
 from app.api.schemas.owner_schemas import OwnerResponse
 from app.utils.enums.car_category import CarCategory
 
-class CarOwnerResponse(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-    id:UUID
-    name:str
-    email:str
 
 class CarWithOwnerResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
